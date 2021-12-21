5e61af62a9dad5d2f7dc551eb161853feea61c9c
alexalextran <alextran8385@gmail.com>
2021-12-21 22:20:00 +1100

    js email code works

---
+++ index.js
@@ -0,0 +1,30 @@
+// template_ivv5v7f
+// service_tj4vxst
+// user_2jIT9NA6dfZ3X4lKgbInB
+
+
+ function contact(event){
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible"
+
+ emailjs 
+       .sendForm(
+       'service_tj4vxst',
+         'template_ivv5v7f',
+        event.target,
+       'user_2jIT9NA6dfZ3X4lKgbInB'
+       
+       ).then(() => {
+           loading.classList.remove("modal__overlay--visible");
+           success.classList += " modal__overlay--visible"
+       }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert("The email service is temporarily unavailable. Please contact directly me @ alextran474@gmail.com")
+       })
+     
+
+}
+    
+        
